a818ec825952aeea946c90f5735a012d948eb666
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-22 16:43:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const numberOfColumns = 7;
+const gameBoard = document.getElementById('gameBoard');
+
+let counter = 0;
+
+let currentPlayer = 'coral';
+let nextPlayer = 'seagreen';
+
+for (let iCol = 0; iCol < numberOfColumns; iCol++) {       // columns
+  
+  let column = document.createElement('div');
+  column.classList.add('column');
+  gameBoard.appendChild(column);
+
+  column.addEventListener('click', function (event) {
+    counter++;
+
+    let clickedColumn = event.target;
+
+    let piece = document.createElement('div');
+    piece.classList.add('dot');
+    piece.classList.add(currentPlayer);
+
+    let newNextPlayer = currentPlayer;
+    currentPlayer = nextPlayer;
+    nextPlayer = newNextPlayer;
+
+    piece.textContent = counter;
+    clickedColumn.appendChild(piece);
+  })
+
+
+}
